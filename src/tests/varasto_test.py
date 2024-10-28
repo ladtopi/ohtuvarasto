ab468f443c5e919fcc20304ba35fb1e7579a9eb7
ladtopi <ladtopi@users.noreply.github.com>
2024-10-28 11:15:21 +0200

    improve tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,6 +13,15 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_uuden_varaston_luonti_virheellisellä_tilavuudella(self):
+        self.assertEqual(Varasto(-10).tilavuus, 0)
+
+    def test_uuden_varaston_luonti_virheellisellä_alkusaldolla(self):
+        self.assertEqual(Varasto(-10, -10).saldo, 0)
+
+    def test_uuden_varaston_alkusaldo_korkeintaan_tilavuus(self):
+        self.assertEqual(Varasto(10, 20).saldo, 10)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -23,6 +32,16 @@ class TestVarasto(unittest.TestCase):
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_lisays_ei_tee_mitaan_negatiiviselle_maaralle(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_korkeintaan_mita_mahtuu(self):
+        self.varasto.lisaa_varastoon(1000)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +57,16 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ottaminen_ei_tee_mitaan_negatiivisella_maaralla(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertEqual(self.varasto.ota_varastosta(-2), 0)
+
+    def test_ottaa_voi_korkeintaan_sen_mita_on(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertEqual(self.varasto.ota_varastosta(1000), 8)
+
+    def test_varasto_merkkijonona(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
